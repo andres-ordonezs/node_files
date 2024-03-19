65341503f719d76bfa9ad34ecacdbdeb734d71a9
andres-ordonezs <andresmw.89@gmail.com>
2024-03-19 17:10:13 -0500

    Step 3 done

--- step3.js
+++ step3.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const fsP = require("fs/promises");
-//TODO: same refactor as step1
+
 /* Function takes one argument, path, and it should read the file with that
 path, and print the contents of that file. */
 async function cat(path) {
@@ -14,7 +14,7 @@ async function cat(path) {
     process.exit(1);
   }
 
-  console.log(contents);
+  return contents;
 }
 
 /** Function takes one argument, a web URL, and logs the contents */
@@ -28,30 +28,42 @@ async function webCat(path) {
     console.log(`${err}`);
     process.exit(1);
   }
-  console.log(html);
+  return html;
 }
 
 /* Writes result into file */
-function writeFile(filePath, fileContent) {
-
+async function writeFile(filePath, fileContent) {
+  try {
+    await fsP.writeFile(filePath, fileContent, "utf8");
+  } catch (err) {
+    console.log(`Couldn't write ${filePath}:`);
+    console.error(`${err}`);
+    process.exit(1);
+  }
 }
 
 /** Grabs path from process.argv, determines if it is a file or web URL, and
  * calls the relevant function to print the contents.
  */
-function main() {
-  // const path = process.argv[2];
-
-  if (process.argv[2] === "--out") {
-
-  }
+async function main() {
+  const path = process.argv[process.argv.length - 1];
+  let result;
 
   if (URL.canParse(path)) {
-    webCat(path);
+    result = await webCat(path);
   }
   else {
-    cat(path);
+    result = await cat(path);
   }
+
+  if (process.argv[2] === "--out") {
+    const writeFilePath = process.argv[3];
+
+    writeFile(writeFilePath, result);
+  } else {
+    console.log(result);
+  }
+
 }
 
 main();
